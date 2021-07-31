9f0ce1d409e8d46fd8b9e8d420fa4f436bced9fa
viniluk <viniluk9@gmail.com>
2021-07-31 01:27:31 -0300

    pequenas alterações

--- src/controllers/TextController.js
+++ src/controllers/TextController.js
@@ -1,13 +1,6 @@
-const axios = require("axios");
-const Text = require("../models/Text");
-
-exports = {
+module.exports = {
   async index(req, res) {
     try {
-      const response = await axios.get(`${process.env.URL_LORIPSUM}`);
-      return res.json({
-        message: "Texto recebido com sucesso",
-      });
     } catch (error) {
       return res.json({ error });
     }
