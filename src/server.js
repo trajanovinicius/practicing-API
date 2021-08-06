916c4364bf04f6bc58b39fcc8d71f963014eb9d0
viniluk <viniluk9@gmail.com>
2021-08-06 00:52:38 -0300

    modificações controller

--- src/server.js
+++ src/server.js
@@ -11,9 +11,7 @@ requireDir("./models");
 
 app.use(express.json());
 
-//app.use(`/api/${process.env.VERSION}`, require("./routes"));
-
 app.listen(
   process.env.PORT,
-  console.log(`Server Started on port${process.env.PORT}`)
+  console.log(`Server Started on port ${process.env.PORT}`)
 );
